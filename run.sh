e9ea41c9d2b3e427c1b41653f7c4bc72144da480
Ilya Rakhlin <i.rakhlin@gmail.com>
2025-05-09 19:49:03 -0400

    wet

--- run.sh
+++ run.sh
@@ -7,8 +7,11 @@ set -e
 bashio::log.info "Loading parameters"
 SUBSCRIPTION_KEY="$(bashio::config 'subscription_key')"
 SERVICE_REGION="$(bashio::config 'service_region')"
+CUSTOM_VOICE="$(bashio::config 'custom-voice')"
+ENDPOINT_ID="$(bashio::config 'endpoint_id')"
+
 
 bashio::log.info "Sub: ${SUBSCRIPTION_KEY}"
 bashio::log.info "Reg: ${SERVICE_REGION}"
 
-python3 ./__main__.py --uri "tcp://0.0.0.0:10200" --subscription-key "${SUBSCRIPTION_KEY}" --service-region "${SERVICE_REGION}" --download-dir /data
+python3 ./__main__.py --uri "tcp://0.0.0.0:10200" --subscription-key "${SUBSCRIPTION_KEY}" --service-region "${SERVICE_REGION}" --custom-voice "${CUSTOM_VOICE}" --endpoint_id "${ENDPOINT_ID}" --download-dir /data
